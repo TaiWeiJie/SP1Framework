38be26dbfca35f6e1a1f8a4d7065b61a6b43cdf4
ER YAN YING, SHANNON <shannoner0419@gmail.com>
2020-08-27 16:54:01 +0800

    Spawn multi crops

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -7,14 +7,21 @@
 #include <iomanip>
 #include <sstream>
 
+#define VK_W 0x57
+#define VK_A 0x41
+#define VK_S 0x53
+#define VK_D 0x44
+
 double  g_dElapsedTime;
 double  g_dDeltaTime;
 SKeyEvent g_skKeyEvent[K_COUNT];
 SMouseEvent g_mouseEvent;
 
 // Game specific variables here
-SGameChar   g_sChar;
-EGAMESTATES g_eGameState = S_SPLASHSCREEN; // initial state
+SGameChar      g_sChar;
+SGameCrop      g_sCrops[20];
+SGameSpiders   g_sSpiders;
+EGAMESTATES    g_eGameState = S_SPLASHSCREEN; // initial state
 
 // Console object
 Console g_Console(80, 25, "SP1 Framework");
@@ -26,24 +33,40 @@ Console g_Console(80, 25, "SP1 Framework");
 // Input    : void
 // Output   : void
 //--------------------------------------------------------------
-void init( void )
+void init(void)
 {
     // Set precision for floating point output
-    g_dElapsedTime = 0.0;    
+    g_dElapsedTime = 0.0;
 
     // sets the initial state for the game
     g_eGameState = S_SPLASHSCREEN;
 
+
     g_sChar.m_cLocation.X = g_Console.getConsoleSize().X / 2;
     g_sChar.m_cLocation.Y = g_Console.getConsoleSize().Y / 2;
     g_sChar.m_bActive = true;
-    // sets the width, height and the font name to use in the console
+
+
+    srand((unsigned)time(0));
+    g_sSpiders.m_cLocation.X = (rand() % 68);
+    g_sSpiders.m_cLocation.Y = (rand() % 22);
+    g_sSpiders.m_bActive = true;
+
+    for (int i = 0; i < 20; i++) 
+    {
+        g_sCrops[i].m_cLocation.X = (rand() % 68);
+        g_sCrops[i].m_cLocation.Y = (rand() % 22);
+        g_sCrops[i].m_bActive = true;
+    }
+    //sets the width, height and the font name to use in the console
     g_Console.setConsoleFont(0, 16, L"Consolas");
 
     // remember to set your keyboard handler, so that your functions can be notified of input events
     g_Console.setKeyboardHandler(keyboardHandler);
     g_Console.setMouseHandler(mouseHandler);
 }
+
+
 
 //--------------------------------------------------------------
 // Purpose  : Reset before exiting the program
@@ -52,7 +75,7 @@ void init( void )
 // Input    : Void
 // Output   : void
 //--------------------------------------------------------------
-void shutdown( void )
+void shutdown(void)
 {
     // Reset to white text on black background
     colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
@@ -73,12 +96,12 @@ void shutdown( void )
 // Input    : Void
 // Output   : void
 //--------------------------------------------------------------
-void getInput( void )
+void getInput(void)
 {
     // resets all the keyboard events
     memset(g_skKeyEvent, 0, K_COUNT * sizeof(*g_skKeyEvent));
     // then call the console to detect input from user
-    g_Console.readConsoleInput();    
+    g_Console.readConsoleInput();
 }
 
 //--------------------------------------------------------------
@@ -95,7 +118,7 @@ void getInput( void )
 // Output   : void
 //--------------------------------------------------------------
 void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent)
-{    
+{
     switch (g_eGameState)
     {
     case S_SPLASHSCREEN: // don't handle anything for the splash screen
@@ -122,12 +145,16 @@ void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent)
 // Output   : void
 //--------------------------------------------------------------
 void mouseHandler(const MOUSE_EVENT_RECORD& mouseEvent)
-{    
+{
     switch (g_eGameState)
     {
     case S_SPLASHSCREEN: // don't handle anything for the splash screen
         break;
     case S_GAME: gameplayMouseHandler(mouseEvent); // handle gameplay mouse event
+        break;
+    case S_GUIDE: gameplayMouseHandler(mouseEvent);
+        break;
+    case S_MENU: gameplayMouseHandler(mouseEvent);
         break;
     }
 }
@@ -149,10 +176,14 @@ void gameplayKBHandler(const KEY_EVENT_RECORD& keyboardEvent)
     {
     case VK_UP: key = K_UP; break;
     case VK_DOWN: key = K_DOWN; break;
-    case VK_LEFT: key = K_LEFT; break; 
-    case VK_RIGHT: key = K_RIGHT; break; 
+    case VK_LEFT: key = K_LEFT; break;
+    case VK_RIGHT: key = K_RIGHT; break;
     case VK_SPACE: key = K_SPACE; break;
-    case VK_ESCAPE: key = K_ESCAPE; break; 
+    case VK_ESCAPE: key = K_ESCAPE; break;
+    case VK_W: key = K_UP; break;
+    case VK_A: key = K_LEFT; break;
+    case VK_S: key = K_DOWN; break;
+    case VK_D: key = K_RIGHT; break;
     }
     // a key pressed event would be one with bKeyDown == true
     // a key released event would be one with bKeyDown == false
@@ -162,7 +193,7 @@ void gameplayKBHandler(const KEY_EVENT_RECORD& keyboardEvent)
     {
         g_skKeyEvent[key].keyDown = keyboardEvent.bKeyDown;
         g_skKeyEvent[key].keyReleased = !keyboardEvent.bKeyDown;
-    }    
+    }
 }
 
 //--------------------------------------------------------------
@@ -206,10 +237,13 @@ void update(double dt)
 
     switch (g_eGameState)
     {
-        case S_SPLASHSCREEN : splashScreenWait(); // game logic for the splash screen
-            break;
-        case S_GAME: updateGame(); // gameplay logic when we are in the game
-            break;
+    case S_SPLASHSCREEN: splashScreenWait(); // game logic for the splash screen
+        break;
+    case S_GAME: updateGame(); // gameplay logic when we are in the game
+        break;
+    case S_GUIDE: updateguide(); // logic for how to play screen
+        break;
+    case S_MENU: updatemenu();   // logic for menu
     }
 }
 
@@ -217,52 +251,97 @@ void update(double dt)
 void splashScreenWait()    // waits for time to pass in splash screen
 {
     if (g_dElapsedTime > 3.0) // wait for 3 seconds to switch to game mode, else do nothing
-        g_eGameState = S_GAME;
+        g_eGameState = S_MENU;
 }
 
 void updateGame()       // gameplay logic
 {
     processUserInput(); // checks if you should change states or do something else with the game, e.g. pause, exit
     moveCharacter();    // moves the character, collision detection, physics, etc
-                        // sound can be played here too.
+    UpdateCrops();                 // sound can be played here too.
 }
 
+
+void UpdateCrops() 
+{
+    for (int i = 0; i < 20; i++) 
+    {
+        g_sCrops[i].m_cLocation.X;
+        g_sCrops[i].m_cLocation.Y;
+    }
+
+}
+
+
 void moveCharacter()
-{    
+{
     // Updating the location of the character based on the key release
     // providing a beep sound whenver we shift the character
-    if (g_skKeyEvent[K_UP].keyReleased && g_sChar.m_cLocation.Y > 0)
+    if (g_skKeyEvent[K_UP].keyDown && g_sChar.m_cLocation.Y > 0)
     {
         //Beep(1440, 30);
-        g_sChar.m_cLocation.Y--;       
+        g_sChar.m_cLocation.Y--;
     }
-    if (g_skKeyEvent[K_LEFT].keyReleased && g_sChar.m_cLocation.X > 0)
+    if (g_skKeyEvent[K_LEFT].keyDown && g_sChar.m_cLocation.X > 0)
     {
         //Beep(1440, 30);
-        g_sChar.m_cLocation.X--;        
+        g_sChar.m_cLocation.X--;
     }
-    if (g_skKeyEvent[K_DOWN].keyReleased && g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 1)
+    if (g_skKeyEvent[K_DOWN].keyDown && g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 1)
     {
         //Beep(1440, 30);
-        g_sChar.m_cLocation.Y++;        
+        g_sChar.m_cLocation.Y++;
     }
-    if (g_skKeyEvent[K_RIGHT].keyReleased && g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1)
+    if (g_skKeyEvent[K_RIGHT].keyDown && g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1)
     {
         //Beep(1440, 30);
-        g_sChar.m_cLocation.X++;        
+        g_sChar.m_cLocation.X++;
     }
     if (g_skKeyEvent[K_SPACE].keyReleased)
     {
-        g_sChar.m_bActive = !g_sChar.m_bActive;        
+        g_sChar.m_bActive = !g_sChar.m_bActive;
     }
 
-   
+
 }
 void processUserInput()
 {
     // quits the game if player hits the escape key
     if (g_skKeyEvent[K_ESCAPE].keyReleased)
-        g_bQuitGame = true;    
+        g_eGameState = S_MENU;
+
+}
+
+// for detecting if user clicks on back button in How To PLay screen
+void GuideInput()
+{
+    if (g_mouseEvent.mousePosition.Y == 23 && g_mouseEvent.mousePosition.X >= 63 && g_mouseEvent.mousePosition.X < 67 &&
+        g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+    {
+        g_eGameState = S_MENU;
+    }
+}
+
+void MenuInput()
+{
+    // quits the game if user clicks on "quit" button
+    if (g_mouseEvent.mousePosition.Y == 13 && g_mouseEvent.mousePosition.X >= 37 && g_mouseEvent.mousePosition.X < 41 &&
+        g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+    {
+        g_bQuitGame = true;
+    }
+    // displays guide for game (Not functional yet)
+    if (g_mouseEvent.mousePosition.Y == 11 && g_mouseEvent.mousePosition.X >= 33 && g_mouseEvent.mousePosition.X < 44 &&
+        g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+    {
+        g_eGameState = S_GUIDE;
+    }
+
+    if (g_mouseEvent.mousePosition.Y == 9 && g_mouseEvent.mousePosition.X >= 36 && g_mouseEvent.mousePosition.X < 41 &&
+        g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+    {
+        g_eGameState = S_GAME;
+    }
 }
 
 //--------------------------------------------------------------
@@ -281,6 +360,10 @@ void render()
     case S_SPLASHSCREEN: renderSplashScreen();
         break;
     case S_GAME: renderGame();
+        break;
+    case S_GUIDE: renderguide();
+        break;
+    case S_MENU: rendermenu();
         break;
     }
     renderFramerate();      // renders debug information, frame rate, elapsed time, etc
@@ -316,12 +399,164 @@ void renderSplashScreen()  // renders the splash screen
 
 void renderGame()
 {
+   
     renderMap();        // renders the map to the buffer first
+    renderCrops();      // renders the mobs into the buffer
+    renderSpiders();    // renders spiders into the buffer next
     renderCharacter();  // renders the character into the buffer
+
 }
+
+//Renders the menu screen
+void rendermenu()
+{
+    const WORD colors[] = {
+        9,26,20,22,31,
+    };
+
+    COORD c;
+
+    c.X = 33;
+    c.Y = 5;
+    colour(colors[4]);
+    g_Console.writeToBuffer(c, "Locust Rush", colors[4]);
+
+    c.X = 36;
+    c.Y = 9;
+    colour(colors[0]);
+    g_Console.writeToBuffer(c, "Start", colors[4]);
+
+    c.X = 33;
+    c.Y = 11;
+    colour(colors[0]);
+    g_Console.writeToBuffer(c, "How to Play", colors[4]);
+
+    c.X = 37;
+    c.Y = 13;
+    colour(colors[4]);
+    g_Console.writeToBuffer(c, "Quit", colors[4]);
+
+    //color change when mouse is over buttons
+    if (g_mouseEvent.mousePosition.Y == 9 && g_mouseEvent.mousePosition.X >= 36 && g_mouseEvent.mousePosition.X < 41)
+    {
+        c.X = 36;
+        c.Y = 9;
+        colour(colors[1]);
+        g_Console.writeToBuffer(c, "Start", colors[1]);
+    }
+
+    if (g_mouseEvent.mousePosition.Y == 11 && g_mouseEvent.mousePosition.X >= 33 && g_mouseEvent.mousePosition.X < 44)
+    {
+        c.X = 33;
+        c.Y = 11;
+        colour(colors[3]);
+        g_Console.writeToBuffer(c, "How to Play", colors[3]);
+    }
+
+    if (g_mouseEvent.mousePosition.Y == 13 && g_mouseEvent.mousePosition.X >= 37 && g_mouseEvent.mousePosition.X < 41)
+    {
+        c.X = 37;
+        c.Y = 13;
+        colour(colors[2]);
+        g_Console.writeToBuffer(c, "Quit", colors[2]);
+    }
+}
+
+void updatemenu()
+{
+    MenuInput();
+    rendermenu();
+}
+
+void updateguide()
+{
+    GuideInput();
+    renderguide();
+}
+
+//renders How To Play screen
+void renderguide()
+{
+    COORD c = g_Console.getConsoleSize();
+    c.Y /= 7;
+    c.X = c.X / 2 - 10;
+    g_Console.writeToBuffer(c, "==Controls==", 31);
+    c.Y += 1;
+    c.X = g_Console.getConsoleSize().X / 2 - 9;
+    g_Console.writeToBuffer(c, "Arrow Keys", 31);
+    c.Y += 1;
+    c.X = g_Console.getConsoleSize().X / 2 - 9;
+    g_Console.writeToBuffer(c, "WASD keys", 31);
+    c.Y += 2;
+    c.X = g_Console.getConsoleSize().X / 2 - 11;
+    g_Console.writeToBuffer(c, "==How to Play==", 31);
+    c.Y += 1;
+    c.X = g_Console.getConsoleSize().X / 2 - 40;
+    g_Console.writeToBuffer(c, "Move your locust to a piece of farmland, it will solwly start to eat the crops.", 31);
+    c.Y += 2;
+    c.X = g_Console.getConsoleSize().X / 2 - 40;
+    g_Console.writeToBuffer(c, "By doing so, it will increase your locust population. The larger the population, the faster you consume crops.", 31);
+    c.Y += 3;
+    c.X = g_Console.getConsoleSize().X / 2 - 40;
+    g_Console.writeToBuffer(c, "Watch out for predators, they will attack your locust population on close promixity. If your whole swarm is eaten, you will lose.", 31);
+    c.Y += 3;
+    c.X = g_Console.getConsoleSize().X / 2 - 40;
+    g_Console.writeToBuffer(c, "Bewarned, increasing your locust population beyond (limit) will result in the pest control being called in, resulting in a defeat.", 31);
+    c.Y += 3;
+    c.X = g_Console.getConsoleSize().X / 2 - 40;
+    g_Console.writeToBuffer(c, "To win, the swarm must consume all the crops on the field in the shortest amount of time, without dying to either predators or pest control.", 31);
+
+    c.X = 63;
+    c.Y = 23;
+    g_Console.writeToBuffer(c, "Back", 15);
+
+    if (g_mouseEvent.mousePosition.Y == 23 && g_mouseEvent.mousePosition.X >= 63 && g_mouseEvent.mousePosition.X < 67)
+    {
+        c.X = 63;
+        c.Y = 23;
+        g_Console.writeToBuffer(c, "Back", 12);
+    }
+
+}
+
+
+void renderCrops()
+{
+    // Draw the location of the spiders
+    for (int i = 0; i < 20; i++) 
+    {
+        g_Console.writeToBuffer(g_sCrops[i].m_cLocation, (char)1, 12);
+    }
+}
+
+
+void renderSpiders()
+{
+    // Draw the location of the spiders
+    WORD charColor = 10;
+    if (g_sSpiders.m_bActive)
+    {
+        charColor = 10;
+    }
+    g_Console.writeToBuffer(g_sSpiders.m_cLocation, (char)7, charColor);
+}
+
+void renderCharacter()
+{
+    // Draw the location of the character
+    WORD charColor = 0x0C;
+    if (g_sChar.m_bActive)
+    {
+        charColor = 224;
+    }
+    g_Console.writeToBuffer(g_sChar.m_cLocation, (char)7, charColor);
+}
+
+
 
 void renderMap()
 {
+
     // Set up sample colours, and output shadings
     const WORD colors[] = {
         32, 130 //32 for green, 130 gray, 110 yellow
@@ -613,19 +848,7 @@ void renderMap()
         colour(colors[1]);
         g_Console.writeToBuffer(c, "  ", colors[1]);
     }
-}
 
-
-
-void renderCharacter()
-{
-    // Draw the location of the character
-    WORD charColor = 0x0C;
-    if (g_sChar.m_bActive)
-    {
-        charColor = 0x0A;
-    }
-    g_Console.writeToBuffer(g_sChar.m_cLocation, (char)1, charColor);
 }
 
 void renderFramerate()
@@ -651,7 +874,7 @@ void renderFramerate()
 void renderInputEvents()
 {
     // keyboard events
-    COORD startPos = {50, 2};
+    COORD startPos = { 50, 2 };
     std::ostringstream ss;
     std::string key;
     for (int i = 0; i < K_COUNT; ++i)
@@ -671,15 +894,10 @@ void renderInputEvents()
             break;
         default: continue;
         }
-        if (g_skKeyEvent[i].keyDown)
-            ss << key << " pressed";
-        else if (g_skKeyEvent[i].keyReleased)
-            ss << key << " released";
-        else
-            ss << key << " not pressed";
+        
 
         COORD c = { startPos.X, startPos.Y + i };
-        g_Console.writeToBuffer(c, ss.str(), 0x17);
+        g_Console.writeToBuffer(c, ss.str(), 23);
     }
 
     // mouse events    
@@ -709,7 +927,7 @@ void renderInputEvents()
     case DOUBLE_CLICK:
         ss.str("Double Clicked");
         g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 4, ss.str(), 0x59);
-        break;        
+        break;
     case MOUSE_WHEELED:
         if (g_mouseEvent.buttonState & 0xFF000000)
             ss.str("Mouse wheeled down");
@@ -717,11 +935,8 @@ void renderInputEvents()
             ss.str("Mouse wheeled up");
         g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 5, ss.str(), 0x59);
         break;
-    default:        
+    default:
         break;
     }
-    
+
 }
-
-
-
--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -5,6 +5,7 @@
 
 extern CStopWatch g_swTimer;
 extern bool g_bQuitGame;
+//extern bool openguide;  may be useless
 
 // struct to store keyboard events
 // a small subset of KEY_EVENT_RECORD
@@ -23,7 +24,6 @@ struct SMouseEvent
     DWORD eventFlags;
 };
 
-
 // Enumeration to store the control keys that your game will have
 enum EKEYS
 {
@@ -40,6 +40,8 @@ enum EKEYS
 enum EGAMESTATES
 {
     S_SPLASHSCREEN,
+    S_MENU,
+    S_GUIDE,
     S_GAME,
     S_COUNT
 };
@@ -51,12 +53,25 @@ struct SGameChar
     bool  m_bActive;
 };
 
-void init        ( void );      // initialize your variables, allocate memory, etc
-void getInput    ( void );      // get input from player
-void update      ( double dt ); // update the game and the state of the game
-void render      ( void );      // renders the current state of the game to the console
-void shutdown    ( void );      // do clean up, free memory
+struct SGameCrop
+{
+    COORD m_cLocation;
+    bool  m_bActive;
+};
 
+
+
+struct SGameSpiders
+{
+    COORD m_cLocation;
+    bool  m_bActive;
+};
+
+void init(void);      // initialize your variables, allocate memory, etc
+void getInput(void);      // get input from player
+void update(double dt); // update the game and the state of the game
+void render(void);      // renders the current state of the game to the console
+void shutdown(void);      // do clean up, free memory
 void splashScreenWait();    // waits for time to pass in splash screen
 void updateGame();          // gameplay logic
 void moveCharacter();       // moves the character, collision detection, physics, etc
@@ -66,10 +81,18 @@ void renderSplashScreen();  // renders the splash screen
 void renderGame();          // renders the game stuff
 void renderMap();           // renders the map to the buffer first
 void renderCharacter();     // renders the character into the buffer
+void UpdateCrops();
+void renderCrops();         // renders crops into the buffer
+void renderSpiders();       // renders spiders into the buffer
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
 void renderInputEvents();   // renders the status of input events
-
+void updateguide();         // Updates guide
+void updatemenu();          // Updates Menu
+void renderguide();         // renders How To Play Screen
+void GuideInput();          // Check for inputs in How To Play screen
+void rendermenu();          // Renders the menu screen
+void MenuInput();           // Check for inputs in main menu
 
 // keyboard and mouse input event managers
 void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent);  // define this function for the console to call when there are keyboard events
